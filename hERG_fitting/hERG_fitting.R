cc0670a3ddbed54e44cd8779c73c6f55b6d08046
Jonathan Fine <jon.a.fine@oulook.com>
2021-10-24 16:53:21 -0400

    Modernize the fitting functions

--- hERG_fitting/hERG_fitting.R
+++ hERG_fitting/hERG_fitting.R
@@ -1,6 +1,6 @@
 # File:         hERG_fitting.R
 # Author:       Kelly Chang
-# Date:         Sep 2017
+# Date:         Oct 2017
 # Version:      1.0
 # 
 # Description:  R script to fit hERG drug binding model parameters to Milnes
@@ -12,161 +12,129 @@
 #               line option "-h".
 #
 
-proc_start<-proc.time()
-
-options(warn=1)
-
-library(optparse)
-
-isWindows<-Sys.info()[["sysname"]]=="Windows"
-
-#--- specify command line arguments
-parser<-OptionParser()
-parser<-add_option(parser, c("-d", "--drug"), type="character", help="Drug name [required]")
-parser<-add_option(parser, c("-i", "--sample"), default="0", help="Bootstrap sample number, range, and/or list of samples to be fitted [default 0 fits to original data]")
-parser<-add_option(parser, c("-s", "--seed"), default=100, type="integer", help="Random seed [default 100]")
-parser<-add_option(parser, c("-c", "--cores"), default=1, type="integer", help="Number of cores to use during fitting [default 1]")
-parser<-add_option(parser, c("-f", "--forking"), default=FALSE, action="store_true", help="Flag to turn on forking for parallelization (not supported in Windows)")
-parser<-add_option(parser, c("-l", "--lambda"), type="integer", help="Population size to use for CMA-ES [default 4+floor(3*log(N))]")
-parser<-add_option(parser, c("-m", "--maxiter"), type="integer", help="Maximum number of generations for CMA-ES [default 100*N^2]")
-parser<-add_option(parser, c("-t", "--tol"), type="double", help="Stopping tolerance for CMA-ES [default 0.5e-12]")
-
-args<-parse_args(parser)
-
-#--- load libraries
-library(cmaes)
-library(parallel)
 library(deSolve)
-print(sessionInfo())
+library(cmaes)
+library(vroom)
+library(furrr)
+library(dplyr, quietly = T, warn.conflicts = F)
 
-#--- required argument
-if(is.null(args$drug)) stop("Missing drug argument!")
-drug<-args$drug
+source("setup_hERG_fitting.R")
+source("funs/deWrapper.R")
 
-#--- parse bootstrap list, ranges
-bootstr<-args$sample
-boot_split<-strsplit(bootstr, ",")[[1]]
-boot_list<-lapply(boot_split,
-                  function(s){
-                      tmp<-as.numeric(strsplit(s, "-")[[1]])
-                      if(length(tmp)==0 || length(tmp)>2 || any(is.na(tmp)))
-                          stop("Bad sample specifications!")
-                      boot_start<-tmp[1]
-                      boot_stop<-ifelse(length(tmp)==2, tmp[2], tmp[1])
-                      if(boot_start>boot_stop)
-                          stop("Invalid sample range!")
-                      seq(boot_start, boot_stop) # step size 1
-                  })
-boot_range<-do.call(c,boot_list)
-
-#--- optional arguments
-seednum<-args$seed
-cores<-args$cores
-usefork<-args$forking
-POP_SIZE<-args$lambda
-MAX_GENERATION<-args$maxiter
-STOPTOL<-args$tol
-
-if(usefork && isWindows){
-    print("Windows system detected! Forking not supported, using sockets instead...")
-    usefork<-FALSE
-}
-
-# cmaes hyperparameters
-ctl_list<-list(vectorized=TRUE)
-if(!is.null(POP_SIZE)){
-    print(sprintf("Using population size: %g",POP_SIZE))
-    ctl_list[["lambda"]]<-POP_SIZE
-}
-if(!is.null(MAX_GENERATION)){
-    print(sprintf("Using number of generations: %g",MAX_GENERATION))
-    ctl_list[["maxit"]]<-MAX_GENERATION
-}
-if(!is.null(STOPTOL)){
-    print(sprintf("Using stopping tolerance: %g",STOPTOL))
-    ctl_list[["stop.tolx"]]<-STOPTOL
-}
-
-#--- setup serial or parallel evaluation
-setupfile<-"setup_hERG_fitting.R"
-usesocket<-FALSE
-if(cores>1){
-    if(usefork){
-        lapplyfun<-function(X, FUN) mclapply(X, FUN, mc.cores=cores, mc.preschedule=FALSE)
-    }else{
-        cl<-makeCluster(cores)
-        invisible(clusterEvalQ(cl,library(deSolve)))
-        clusterExport(cl,c("setupfile","drug","isWindows"))
-
-        lapplyfun<-function(X, FUN) clusterApply(cl, X, FUN)
-        usesocket<-TRUE
-    }
-}else{
-    lapplyfun<-lapply
-}
-
-#--- run range of bootstraps
-for(boot_num in boot_range){
-    print(sprintf("Running %s bootstrap sample %d on %d cores", drug, boot_num, cores))
-
-    #--- set seed for reproducibility
-    set.seed(seednum, kind="L'Ecuyer-CMRG")
-
+hERG_fitting <- function(
+    drug,
+    drug_boots,
+    boot_num,
+    params = NULL,
+    seed = 100,
+    cores = 1,
+    ctl_list=list(
+        lambda = 80,
+        stop.tolx = 0.001
+    )
+    ) {
+    
+    ctl_list$vectorized <- TRUE
+    
     #---setup script
-    source(setupfile)
-    source("funs/deWrapper.R")
-
+    Rcpp::sourceCpp("models/hergmod.cpp")
+    
+    print(sprintf("Running %s bootstrap sample %d on %d cores", drug, boot_num, cores))
+    
+    #--- set seed for reproducibility
+    set.seed(seed, kind="L'Ecuyer-CMRG")
+    
+    setup <- setup_hERG_fitting(drug, boot_num, drug_boots)
+    
     #--- initial parameter values
-    if(boot_num>0){
-        pardf<-read.table(sprintf("results/%s/pars.txt",drug)) # start from optimal values
-        initval<-setNames(pardf[,2],pardf[,1])
-        initpar<-encode_pars(initval[pnames]) # pnames from setupfile
+    if(!is.null(params)){
+        # start from optimal values
+        initpar<-encode_pars(params[setup$pnames], setup$pmax, setup$pp$Low, setup$pp$High)
     }else{
-        initpar<-runif(length(pnames))*pmax # pnames,pmax from setupfile
+        # pnames,pmax from setupfile
+        initpar<-runif(length(setup$pnames)) * setup$pmax
     }
-
-    #--- output directory
-    outdir<-paste0("results/",drug,"/")
-    if(boot_num>0)
-        outdir<-paste0(outdir,sprintf("boot/%05d/",boot_num))
-    system(paste0("mkdir -p ",outdir))
-    print(outdir)
-
-    #--- update setup on cluster
-    if(usesocket){
-        clusterExport(cl,"boot_num")
-        invisible(clusterEvalQ(cl, source(setupfile)))
-    }
-
-    #--- prepare vectorized objective function
-    genfile<-paste0(outdir,"CMAESgeneration")
-    errfile<-paste0(outdir,"CMAESerror")
-    if(file.exists(genfile)) file.remove(genfile);
-    if(file.exists(errfile)) file.remove(errfile);
-    objfun_vec<-function(pop){
+    
+    objfun_vec <- function(pop){
         nchr<-ncol(pop)
         chrpernode<-ceiling(nchr/cores)
         nnodes<-min(cores,nchr)
         print(Sys.time())
-        errorlist<-lapplyfun(1:nnodes, deWrapper(chrpernode,pop,objfun)) # objfun defined in setupfile
-        errormat<-do.call(rbind, errorlist)
-        imin<-which.min(errormat[,2])
-        write.table(data.frame(t(decode_pars(pop[,imin]))), genfile, sep=" ", row.names=F, col.names=F, append=T) #turned off writing might speed it up
-        write(errormat[imin,2], errfile, ncolumns=1, sep=" ", append=T)
-        errormat[,2]
+        
+        # objfun defined in setupfile
+        errors <- future_map_dfr(
+            1:nnodes,
+            deWrapper(chrpernode, pop, objfun, setup),
+            .options = furrr_options(
+                seed = NULL,
+                globals = c(
+                    "setup",
+                    "objfun",
+                    "pop",
+                    "chrpernode",
+                    "run_sims",
+                    "decode_pars",
+                    "derivs"
+                )
+            )
+        )
+        
+        imin<-which.min(errors$errors)
+
+        print(t(decode_pars(pop[,imin], setup$pmax, setup$pp$Low, setup$pp$High)))
+        print(errors$errors[imin])
+        
+        errors$errors
     }
-
+    
     #--- run CMA-ES
-    res<-cma_es(initpar,objfun_vec,lower=0,upper=pmax,control=ctl_list) # pmax from setupfile
-    str(res)
-
+    res <- cma_es(initpar,objfun_vec,lower=0,upper=setup$pmax,control=ctl_list)
+    
     #--- save best results
-    pars<-signif(decode_pars(res$par), digits=4)
-    names(pars)<-pnames
-    write.table(pars, file=paste0(outdir,"pars.txt"), row.names=T, col.names=F, quote=F)
+    pars<-signif(decode_pars(res$par, setup$pmax, setup$pp$Low, setup$pp$High), digits=4)
+    names(pars) <- setup$pnames
+    
+    pars
+}
 
-}# for boot_num
+hERG_fitting_initial <- function(
+    drug,
+    drug_boots,
+    boot_num = 0,
+    seed = 100,
+    cores = 1,
+    ctl_list=list(
+        lambda = 80,
+        stop.tolx = 0.001
+    )
+) {
+    hERG_fitting(drug, drug_boots, boot_num, NULL, seed, cores, ctl_list)
+}
 
-if(usesocket) stopCluster(cl)
+hERG_fitting_boot <- function(
+    drug,
+    params,
+    drug_boots,
+    boot_num = 1:drug_boots[[drug]]$R,
+    seed = 100,
+    cores = 1,
+    ctl_list=list(
+        lambda = 80,
+        stop.tolx = 0.001
+    )
+    ) {
+    
+    #--- run range of bootstraps
+    purrr::map_dfr(boot_num, function(d) {
+        hERG_fitting(drug, drug_boots, d, params, seed, cores, ctl_list)
+    }, .id = "boot")
+}
 
-print(proc.time()-proc_start)
+read_pars_txt <- function(file) {
+    res <- read.table(file)
+    
+    r <- res$V2
+    names(r) <- res$V1
+    
+    r
+}
--- hERG_fitting/setup_hERG_fitting.R
+++ hERG_fitting/setup_hERG_fitting.R
@@ -1,103 +1,137 @@
 # File:         setup_hERG_fitting.R
 # Author:       Kelly Chang
 #               Zhihua Li
-# Date:         Sep 2017
+# Date:         Oct 2017
 # Version:      1.0
 # 
 # Description:  R helper script to setup fitting for the hERG drug binding
 #               model.
 #
 
-#--- code to setup simulations
-nnnfile<-"models/hergmod_drug_param_bounds.txt"
-modelname<-"hergmod"
-modeldir<-"models/"
 source("funs/get_boot_data.R")
 source("funs/stepprotocol.R")
 source("funs/model_init.R")
 
-#--- load ODE model, states, and parameters
-extension<-ifelse(isWindows, ".dll", ".so")
-dyn.load(paste0(modeldir,modelname,extension))
-tmp<-read.table(paste0(modeldir,modelname,"_states.txt"), col.names=c("param","value"))
-states<-setNames(tmp$value, tmp$param)
-tmp<-read.table(paste0(modeldir,modelname,"_pars.txt"), col.names=c("param","value"))
-pars<-setNames(tmp$value, tmp$param)
+encode_pars<-function(pars,pmax,low,high) pmax*log10(pars/low)/log10(high/low)
+decode_pars<-function(ind,pmax,low,high) low*(high/low)^(ind/pmax)
 
-#--- parameters to be fitted
-pp<-read.table(nnnfile,header=T,as.is=T)
-pnames<-pp$Parameter
-high_bounds<-pp$High
-low_bounds<-pp$Low
+setup_hERG_fitting <- function(drug, boot_num, drug_boots = NULL) {
 
-#--- parameter encoding to 0-10 range
-pmax<-10
-encode_pars<-function(pars) pmax*log10(pars/low_bounds)/log10(high_bounds/low_bounds)
-decode_pars<-function(ind) low_bounds*(high_bounds/low_bounds)^(ind/pmax)
-
-#--- setup model
-pars["T"]<-37
-nbeats<-10
-
-dummy<-stepprotocol(-80, 900,-80,40,0,10000,14060)
-fulltimes<-dummy[[1]]
-peaktimes<-dummy[[2]]
-eventdata<-dummy[[3]]
-
-hergmodel<-model_init(modelname, states, pars, pnames, fulltimes, eventdata, nbeats)
-ctlsweeps<-hergmodel$controlsweeps()
-
-#--- get bootstrap indices
-boot.out<-readRDS(sprintf("results/%s/boot_out.rds",drug))
-boot_idx<-boot::boot.array(boot.out, indices=TRUE)
-if(boot_num>0){
-    idx<-boot_idx[boot_num,]
-}else{
-    idx<-1:ncol(boot_idx)
+    #--- code to setup simulations
+    nnnfile<-"models/hergmod_drug_param_bounds.txt"
+    modelname<-"hergmod"
+    modeldir<-"models/"
+    
+    #--- load ODE model, states, and parameters
+    tmp<-read.table(paste0(modeldir,modelname,"_states.txt"), col.names=c("param","value"))
+    states<-setNames(tmp$value, tmp$param)
+    
+    tmp<-read.table(paste0(modeldir,modelname,"_pars.txt"), col.names=c("param","value"))
+    pars<-setNames(tmp$value, tmp$param)
+    
+    #--- parameters to be fitted
+    pp<-read.table(nnnfile,header=T,as.is=T)
+    pnames<-pp$Parameter
+    high_bounds<-pp$High
+    low_bounds<-pp$Low
+    
+    #--- parameter encoding to 0-10 range
+    pmax <- 10
+    
+    #--- setup model
+    pars["T"] <- 37
+    nbeats <- 10
+    
+    dummy<-stepprotocol(-80, 900, -80, 40, 0, 10000, 14060)
+    fulltimes<-dummy[[1]]
+    peaktimes<-dummy[[2]]
+    eventdata<-dummy[[3]]
+    
+    hergmodel<-model_init(modelname, states, pars, pnames, fulltimes, eventdata, nbeats)
+    ctlsweeps<-hergmodel$controlsweeps()
+    
+    #--- get bootstrap indices
+    drug_boot <- drug_boots[[drug]]
+    
+    boot_idx<-boot::boot.array(drug_boot, indices=TRUE)
+    
+    if(boot_num>0){
+        idx<-boot_idx[boot_num,]
+    }else{
+        idx<-1:ncol(boot_idx)
+    }
+    
+    print(idx)
+    
+    #--- get data
+    fracdata<-get_boot_data(paste0("data/",drug,".csv.xz"), idx)
+    concvec<-names(fracdata) # note these are characters!
+    nsweeps<-max(sapply(fracdata, length))
+    if(nbeats!=nsweeps)
+        stop(sprintf("Simulations have %d beats, but experiments have %d sweeps!",nbeats,nsweeps))
+    
+    #--- check timepoints
+    haspts<-sapply(fracdata,
+                   function(x){
+                       sapply(1:min(length(x),nbeats),
+                              function(i){
+                                  all(x[[i]][,"time"]%in%ctlsweeps[[i]][,"time"])
+                              })
+                   })
+    if(is.list(haspts)) haspts<-do.call(c,haspts) # for missing data
+    if(!all(haspts)) stop("Simulation timepoints don't match data!")
+    
+    list(
+        fracdata = fracdata,
+        pnames = pnames,
+        pmax = pmax,
+        hergmodel = hergmodel,
+        pp = pp,
+        nbeats = 10
+    )
 }
-print(idx)
-
-#--- get data
-fracdata<-get_boot_data(paste0("data/",drug,".csv"), idx)
-concvec<-names(fracdata) # note these are characters!
-nsweeps<-max(sapply(fracdata, length))
-if(nbeats!=nsweeps) stop(sprintf("Simulations have %d beats, but experiments have %d sweeps!",nbeats,nsweeps))
-
-#--- check timepoints
-haspts<-sapply(fracdata,
-               function(x){
-                   sapply(1:min(length(x),nbeats),
-                          function(i){
-                              all(x[[i]][,"time"]%in%ctlsweeps[[i]][,"time"])
-                          })
-               })
-if(is.list(haspts)) haspts<-do.call(c,haspts) # for missing data
-if(!all(haspts)) stop("Simulation timepoints don't match data!")
 
 #--- function for running drug simulations
-run_sims<-function(fitpars){
+run_sims<-function(fitpars, concvec, hergmodel){
     alldrugsweeps<-list()
+
     for(conc in concvec){
-        drugsweeps<-hergmodel$run_drug(fitpars, as.numeric(conc)) # note conversion to numeric
+
+        drugsweeps <- hergmodel$run_drug(fitpars, as.numeric(conc)) # note conversion to numeric
+        
         if(length(drugsweeps)==0)
             return(c())
+        
         alldrugsweeps[[conc]]<-drugsweeps
     }
     alldrugsweeps
 }
 
 #--- define objective function
-objfun<-function(ind){
+objfun<-function(ind, setup){
+    if (.Platform$OS.type == "windows"){
+        library(deSolve)
+        source("models/hergmod.cpp.R")
+    }
+
     # run simulations
-    alldrugsweeps<-run_sims(decode_pars(ind))
+    fracdata <- setup$fracdata
+    concvec <- names(fracdata)
+    
+    decoded_pars <- decode_pars(ind, setup$pmax, setup$pp$Low, setup$pp$High)
+
+    alldrugsweeps <- run_sims(decoded_pars, concvec, setup$hergmodel)
+
     if(length(alldrugsweeps)==0)
         return(1e50)
 
+    ctlsweeps <- setup$hergmodel$controlsweeps()
     # compute errors
     idxV<-which(colnames(ctlsweeps[[1]])=="V")
     fval<-0
     yPred<-list()
     yO<-list()
+    
     for(conc in concvec){
         yPred[[conc]]<-list()
         yO[[conc]]<-list()
@@ -112,7 +146,7 @@ objfun<-function(ind){
         }
 
         # negative constraints violation
-        allval<-alldrugsweeps[[conc]][[nbeats]][,c(-1,-idxV)]
+        allval<-alldrugsweeps[[conc]][[setup$nbeats]][,c(-1,-idxV)]
         negativeerror<-sum(apply(allval,2,function(x) mean(pmin(x,0)^2)))
     }
 
@@ -141,9 +175,9 @@ objfun<-function(ind){
     }
 
     specerr<-0
-    if(nrow(firstO)==nbeats){
-        yPdiff1<-(firstP[1,]-firstP[nbeats,])/firstP[1,]
-        yOdiff1<-(firstO[1,]-firstO[nbeats,])/firstO[1,]
+    if(nrow(firstO)==setup$nbeats){
+        yPdiff1<-(firstP[1,]-firstP[setup$nbeats,])/firstP[1,]
+        yOdiff1<-(firstO[1,]-firstO[setup$nbeats,])/firstO[1,]
         yOdiff1<-pmax(0, yOdiff1)
         nabeat<-is.na(yOdiff1) | is.na(yPdiff1)
         specerr<-specerr+sum(0.2*numpt/length(concvec)*(yPdiff1-yOdiff1)[!nabeat & significantidx]^2)
